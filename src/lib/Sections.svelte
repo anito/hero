be17a7b04b29b0b7ab78c20890ab935530055114
Axel Nitzschner <axelnitzschner@mac.com>
2023-04-26 17:12:58 +0200

    changes

--- src/lib/Sections.svelte
+++ src/lib/Sections.svelte
@@ -105,6 +105,10 @@
             invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
             accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
             sanctus est Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+            sanctus est Lorem ipsum dolor sit amet.
           </p>
           <a class="read-more" href="#neufahrzeuge" target="_self" role="button">Mehr erfahren</a>
         </div>
